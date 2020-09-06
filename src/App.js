0bfd082fcb54e9b7b755d0b997b02a7d89b9a5b8
JHugoSilva <hugosilvahs31@gmail.com>
2020-09-06 11:14:37 -0300

    Clone da Netflix

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import Tmdb from './Tmdb'
+import MovieRow from './components/MovieRow'
+import FeaturedMovie from './components/FeaturedMovie'
+import Header from './components/Header'
+import './App.css'
+export default () => {
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [ movieList , setMovieList ] = useState([])
+  const [ featuredData , setfeaturedData ] = useState(null)
+  const [ blackHeader, setBlackHeader ] = useState(false)
+  useEffect(()=>{
+    const loadAll = async () => {
+      let list = await Tmdb.getHomeList()
+      setMovieList(list)
+      
+      let originals = list.filter(i=>i.slug === 'originals')
+      let randomChosen = Math.floor(Math.random() * (originals[0].items.results.length - 1))
+      let chosen = originals[0].items.results[randomChosen]
+      let chosenInfo = await Tmdb.getMovieInfo(chosen.id, 'tv')
+      setfeaturedData(chosenInfo)
+    }
+
+    loadAll()
+  },[])
+
+  useEffect(()=>{
+    const scrollListener = () => {
+      if(window.scrollY > 10){
+        setBlackHeader(true)
+      } else {
+        setBlackHeader(false)
+      }
+    }
+    window.addEventListener('scroll', scrollListener)
+    return () => {
+      window.removeEventListener('scroll', scrollListener)
+    }
+  },[])
+  return(
+    <>
+    <Header black={blackHeader}/>  
+      <div className="page">
+        {featuredData && <FeaturedMovie item={featuredData}/>}
+        
+        <section className="lists">
+        {movieList.map((item, key)=>(
+            <MovieRow key={key} title={item.title} items={item.items}/>
+          ))}
+        </section>
+        <footer>
+        Feito em Live (https://www.youtube.com/watch?v=tBweoUiMsDg) para estudo de react, todos os direitos das imagens são da Netflix.
+        Dados Extraidos de https://www.themoviedb.org/
+        </footer>
+
+        {movieList.length <= 0 &&
+        <div className="loading">
+          <img src="https://www.filmelier.com/pt/br/news/wp-content/uploads/2020/03/netflix-loading.gif" alt="Carregando ..."/>        
+        </div>
+        }
+      </div>
+    </>
+  )
 }
-
-export default App;
